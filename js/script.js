ab70679f88513c152cd27e7b73b3afe682cc305d
Rizvan Dev <rizvan.ahmedov@outlook.com>
2021-07-08 17:10:13 +0400

    Added logic, minified images

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const home = document.getElementById('home');
 const homeAway = document.getElementById('home-away');
 const cave = document.getElementById('cave');
 const modal = document.querySelector('.modal');
+const modalgg = document.querySelector('.modal-gg');
+const modalwp = document.querySelector('.modal-wp');
 
 document.addEventListener('click', function (event) {
    jump();
@@ -29,7 +31,7 @@ let isAlife = setInterval(function () {
    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
    let cactus2Left = parseInt(window.getComputedStyle(cactus2).getPropertyValue('left'));
 
-   if (cactus2Left < 58 && cactus2Left > 0 && dinoTop >= 148) {
+   if (cactus2Left < 58 && cactus2Left > 0 && dinoTop >= 238) {
       cactus2.style.animationPlayState = "paused";
       cactus1.style.animationPlayState = "paused";
       home.style.animationPlayState = "paused";
@@ -41,4 +43,28 @@ let isAlife = setInterval(function () {
       dino.style.animationPlayState = 'paused', dino.style.transform = 'rotate(-75deg)', dino.style.transition = '0.2s';
    };
 
-}, 1);
+   let caveElemenStyle = parseInt(window.getComputedStyle(cave).getPropertyValue('left'));
+
+   if (caveElemenStyle == 800) {
+      cactus2.style.animationPlayState = "paused";
+      cactus1.style.animationPlayState = "paused";
+      home.style.animationPlayState = "paused";
+      homeAway.style.animationPlayState = "paused";
+      road.style.animationPlayState = "paused";
+      road2.style.animationPlayState = "paused";
+      dino.classList.remove('jump');
+      dino.classList.add('dino-go-home');
+      modalgg.style.opacity = '1', modalgg.style.transition = '1.2s ease-in';
+
+      if (modalgg.style.opacity = '1') {
+         modalwp.style.top = '7%';
+      };
+   };
+
+   let dinoElementStyle = parseInt(window.getComputedStyle(dino).getPropertyValue('left'));
+
+   if (dinoElementStyle == 660) {
+      dino.style.display = "none";
+   };
+
+}, 10);
